3d20ab1fd7484588f6a956ade8651131fb6abdfb
Raivis Filimonovs <raivisfilimonovs@gmail.com>
2023-10-19 21:57:50 +0300

    Add remote fetch functionality, basic tabling and pagination, searching and filtering

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import ShowJsonTable from "./components/JsonTable";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>COVID-19 Data</h1>
+        <ShowJsonTable />
+    </div>
+  );
+}
+
+export default App;
